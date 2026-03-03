71cb1c0ee30131a3999b3a7bdefa8e58ed2d9a28
rutrum <dave@rutrum.net>
2026-03-03 08:46:20 -0500

    No more submodules, all nix

--- flake.nix
+++ flake.nix
@@ -2,22 +2,27 @@
   description = "rutrum.net";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    typeset = {
+      url = "github:rutrum/typeset";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs }: 
-  let 
+  outputs = { self, nixpkgs, typeset }:
+  let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-  in 
+  in
     {
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         pname = "rutrum.net";
         version = "0.1.0";
-        src = builtins.fetchGit {
-          url = ./.;
-          submodules = true;
-        };
+        src = ./.;
         nativeBuildInputs = with pkgs; [ zola ];
-        buildPhase = "zola build";
+        buildPhase = ''
+          mkdir -p themes
+          ln -s ${typeset} themes/typeset
+          zola build
+        '';
         installPhase = "cp -r public $out";
       };
       
